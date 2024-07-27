d2d877c1867ad9fe3f11269bb25463ddcf4eee36
Dollynt <luiz.guilherme1319@gmail.com>
2024-07-27 12:43:50 -0300

    add alert dialog callback to exit

--- app/src/main/java/com/example/loginproject/SecondActivity.kt
+++ app/src/main/java/com/example/loginproject/SecondActivity.kt
@@ -1,12 +1,19 @@
 package com.example.loginproject
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import com.example.loginproject.databinding.ActivitySecondBinding
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            showExitConfirmationDialog()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
@@ -14,10 +21,18 @@ class SecondActivity : AppCompatActivity() {
 
         val user = "${getString(R.string.user)} ${intent.getStringExtra("USER")}"
         val dateHour = "${getString(R.string.logged_in)} ${intent.getStringExtra("DATE_HOUR")}"
-        println(user)
 
         binding.userTxt.text = user
-        println(binding.userTxt.text)
         binding.loggedIn.text = dateHour
+
+        onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private fun showExitConfirmationDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("Are you sure you want to exit?")
+            .setPositiveButton("Yes") { _, _ -> finish() }
+            .setNegativeButton("No", null)
+            .show()
     }
 }
