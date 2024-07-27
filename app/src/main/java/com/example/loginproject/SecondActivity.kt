442c0cf16fc443c1818acd7eb9cb7737b51f660d
Dollynt <luiz.guilherme1319@gmail.com>
2024-07-27 13:42:41 -0300

    add submenu with explicit intent

--- app/src/main/java/com/example/loginproject/SecondActivity.kt
+++ app/src/main/java/com/example/loginproject/SecondActivity.kt
@@ -1,8 +1,12 @@
 package com.example.loginproject
 
 import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loginproject.databinding.ActivitySecondBinding
@@ -20,6 +24,8 @@ class SecondActivity : AppCompatActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setTitle("Mobile Development")
+
         val user = "${getString(R.string.user)} ${intent.getStringExtra("USER")}"
         val dateHour = "${getString(R.string.logged_in)} ${intent.getStringExtra("DATE_HOUR")}"
 
@@ -32,6 +38,32 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_default, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.option1 -> {
+                Toast.makeText(this, "option1 was selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.sub1 -> {
+                val url = "https://portal.cin.ufpe.br/"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+                true
+            }
+            R.id.sub2 -> {
+                val url = "https://play.kotinlang.org/"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+                true
+            }
+            R.id.option2 -> {
+                Toast.makeText(this, "option2 was selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun showExitConfirmationDialog() {
